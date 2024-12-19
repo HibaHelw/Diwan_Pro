80f4f5bc81a2f116b727a8a4f4e883bb922d29c8
Hiba <52370597+HibaHelw@users.noreply.github.com>
2024-12-19 11:42:05 +0200

    Diwan Controllers

---
+++ DiwanControllers/ModelsControllers/BaseController.cs
@@ -0,0 +1,59 @@
+﻿using DiwanModels.Models.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiwanModels.Context;
+
+namespace DiwanControllers.ModelsControllers
+{
+    public class BaseController<T> where T : BaseModel
+    {
+        protected readonly DiwanContext _context;
+
+        public BaseController()
+        {
+            _context = new DiwanContext();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().Where(c => c.IsExist == true).ToList();
+        }
+
+        public T GetEntityById(int id)
+        {
+            return _context.Set<T>().FirstOrDefault(c => c.ID == id && c.IsExist == true);
+        }
+        public void AddOrUpdate(T entity)
+        {
+            if (entity.ID == 0)
+            {
+                _context.Set<T>().Add(entity);
+            }
+            else
+            {
+                entity.UpdatedAt = DateTime.Now;
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Remove object softly 
+        /// </summary>
+        /// <param name="model"></param>
+        public void Remove(T entity)
+        {
+            var entityFromDb = _context.Set<T>().Find(entity.ID);
+            if (entityFromDb != null)
+            {
+                entityFromDb.DeletedAt = DateTime.Now;
+                entityFromDb.IsExist = false;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DiwanControllers/ModelsControllers/PostController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiwanModels.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiwanControllers.ModelsControllers
+{
+    public class PostController : BaseController<Post>
+    {
+
+        public List<Post> GetAllPosts(int year)
+        {
+            return _context.Posts.Include(c => c.PostType).Include(c => c.PostState)
+                .Include(c => c.Sender)
+                .Where(c => c.IsExist == true && c.Year == year).ToList();
+        }
+
+        public List<Post> GetPostsWithType(int typeId)
+        {
+            return _context.Posts.Where(c => c.IsExist == true
+                                             && c.PostTypeId == typeId).ToList();
+        }
+        public List<Post> GetPostsWithState(int stateId)
+        {
+            return _context.Posts.Where(c => c.IsExist == true
+                                             && c.PostStateId == stateId).ToList();
+        }
+
+        public List<Post> GetPostsInYear(int year)
+        {
+            return _context.Posts.Where(c => c.IsExist == true
+                                             && c.Year == year).ToList();
+        }
+
+        public List<PostEmployee> GetPostReceivers(int postId)
+        {
+            return _context.PostEmployees.Where(c => c.PostId == postId).ToList();
+        }
+        public void DeletePostReceivers(int postId)
+        {
+            var receivers = _context.PostEmployees.Where(c => c.PostId == postId).ToList();
+            _context.PostEmployees.RemoveRange(receivers);
+            _context.SaveChanges();
+        }
+        public void AddPostReceivers(int postId, List<int> receivers)
+        {
+            foreach (var rec in receivers)
+            {
+                var postEmployee = new PostEmployee()
+                {
+                    ReceiverId = rec,
+                    PostId = postId,
+                    State = "",
+                };
+                _context.PostEmployees.Add(postEmployee);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
